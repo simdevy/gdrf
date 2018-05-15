d9c4e9d89decd5e3cbc8f9d8aa348d03398ef15b
Simon Hall <me@simons-imac.home>
2018-05-15 21:53:38 +0200

    V3

--- app.js
+++ app.js
@@ -4,7 +4,21 @@
 
     angular.module('myFirstApp', [])
     .controller('MyFirstController', function($scope){
-        $scope.name ="Simon";
+        $scope.name = "";
+        $scope.totalValue = 0;
+
+        $scope.displayNumeric = function () {
+            var totalNameValue = calculateNumericForString($scope.name);
+            $scope.totalValue = totalNameValue;
+        };
+
+        function calculateNumericForString(string) {
+            var totalStringValue = 0;
+            for (var i = 0; i < string.length; i++){
+                totalStringValue += string.charCodeAt(i);
+            }
+            return totalStringValue;
+        }
 });
 
 })();
